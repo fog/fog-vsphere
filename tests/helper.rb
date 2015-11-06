d417a1f7d66664ceaabea62ff913e9d848fac9f6
J.R. Garcia <jrg@vmware.com>
2015-11-06 08:49:56 -0600

    Add Coveralls support to tests
    Add code to run Coveralls when tests are run

--- tests/helper.rb
+++ tests/helper.rb
@@ -1,1 +1,14 @@
+if ENV['COVERAGE'] == 'true'
+  require 'coveralls'
+  require 'simplecov'
+  SimpleCov.command_name "shindo:#{Process.pid.to_s}"
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+  SimpleCov.merge_timeout 3600
+
+  Coveralls.wear!
+end
+
 require File.expand_path('../../lib/fog/vsphere', __FILE__)
