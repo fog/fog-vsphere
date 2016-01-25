7ebb62cd0962701de7e3d388af86df138d25851e
Timo Goebel <timo.goebel@dm.de>
2016-01-25 10:07:23 +0100

    improve upload_iso

--- lib/fog/vsphere/requests/compute/upload_iso.rb
+++ lib/fog/vsphere/requests/compute/upload_iso.rb
@@ -19,14 +19,15 @@ module Fog
         def upload_iso(options = {})
           options = upload_iso_check_options(options)
           datastore = get_raw_datastore(options['datastore'], options['datacenter'])
-          datacenter = get_datacenter(options['datacenter'])
+          datacenter = get_raw_datacenter(options['datacenter'])
           filename = options['filename'] || File.basename(options['local_path'])
           unless datastore.exists? options['upload_directory']+'/'
-            @connection.serviceContent.fileManager.MakeDirectory :name => "[#{options['datastore']}] #{options['directory']}",
+            @connection.serviceContent.fileManager.MakeDirectory :name => "[#{options['datastore']}] #{options['upload_directory']}",
                                                                  :datacenter => datacenter,
                                                                  :createParentDirectories => false
           end
           datastore.upload options['upload_directory']+'/'+filename, options['local_path']
+          datastore.exists? options['upload_directory']+'/'+filename
         end
       end
     end
