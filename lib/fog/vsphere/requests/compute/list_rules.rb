64ecaf1ffbeda07f2491918180abe6bac6b9a978
c0puga <c0puga@walmart.com>
2016-09-15 16:01:52 -0500

    list_rules, adding the ability to also list ClusterVmHostRuleInfo type rules.

--- lib/fog/vsphere/requests/compute/list_rules.rb
+++ lib/fog/vsphere/requests/compute/list_rules.rb
@@ -8,17 +8,23 @@ module Fog
         end
 
         protected
-      
+
         def rule_attributes(rule, filters)
-          {
-            datacenter: filters[:datacenter],
-            cluster:    filters[:cluster],
-            key:        rule[:key],
-            name:       rule[:name],
-            enabled:    rule[:enabled],
-            type:       rule.class,
-            vm_ids:     rule[:vm].map {|vm| vm.config.instanceUuid }
-          }
+          attributes = {}
+          attributes[:datacenter] = filters[:datacenter]
+          attributes[:cluster] = filters[:cluster]
+          attributes[:key] = rule[:key]
+          attributes[:name] = rule[:name]
+          attributes[:enabled] = rule[:enabled]
+          attributes[:type] = rule.class
+          if rule.class.to_s == 'ClusterAntiAffinityRuleSpec' || rule.class.to_s == 'ClusterAffinityRuleSpec'
+            attributes[:vm_ids] = rule[:vm].map {|vm| vm.config.instanceUuid}
+          elsif rule.class.to_s == 'ClusterVmHostRuleInfo'
+            attributes[:mandatory] = rule[:mandatory]
+            attributes[:vmGroupName] = rule[:vmGroupName]
+            attributes[:affineHostGroupName] = rule[:affineHostGroupName]
+          end
+          return attributes
         end
       end
       class Mock
