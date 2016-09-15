a2336f328a93ef7c051bc9185c4943273444b0e6
c0puga <c0puga@walmart.com>
2016-09-15 13:58:41 -0500

    adding the ability to create ClusterVmHostRuleInfo type DRS rule.

--- lib/fog/vsphere/requests/compute/create_rule.rb
+++ lib/fog/vsphere/requests/compute/create_rule.rb
@@ -3,26 +3,34 @@ module Fog
     class Vsphere
       class Real
         def create_rule(attributes={})
+          raise ArgumentError, 'type attribute is required' if attributes[:type].nil?
           cluster = get_raw_cluster(attributes[:cluster], attributes[:datacenter])
-          # Check if it already exists and blow up if it does
-          # (otherwise ESX just happily accepts it and then considers it a conflict)
           rule = cluster.configurationEx.rule.find {|n| n[:name] == attributes[:name]}
           if rule
             raise ArgumentError, "Rule #{attributes[:name]} already exists!"
           end
-          # First, create the rulespec
-          vms = attributes[:vm_ids].to_a.map {|id| get_vm_ref(id, attributes[:datacenter])}
-          spec = attributes[:type].new(
-            name: attributes[:name],
-            enabled: attributes[:enabled],
-            vm: vms
-          )
+          if (attributes[:type].to_s == 'ClusterAntiAffinityRuleSpec' || attributes[:type].to_s == 'ClusterAffinityRuleSpec')
+            vms = attributes[:vm_ids].to_a.map {|id| get_vm_ref(id, attributes[:datacenter])}
+            spec = attributes[:type].new(
+                name: attributes[:name],
+                enabled: attributes[:enabled],
+                vm: vms
+            )
+          elsif attributes[:type].to_s == 'ClusterVmHostRuleInfo'
+            spec = attributes[:type].new(
+                name: attributes[:name],
+                enabled: attributes[:enabled],
+                mandatory: attributes[:mandatory],
+                vmGroupName: attributes[:vmGroupName],
+                affineHostGroupName: attributes[:affineHostGroupName]
+            )
+          end
           # Now, attach it to the cluster
           cluster_spec = RbVmomi::VIM.ClusterConfigSpecEx(rulesSpec: [
-            RbVmomi::VIM.ClusterRuleSpec(
-              operation: RbVmomi::VIM.ArrayUpdateOperation('add'),
-              info: spec
-            )
+              RbVmomi::VIM.ClusterRuleSpec(
+                  operation: RbVmomi::VIM.ArrayUpdateOperation('add'),
+                  info: spec
+              )
           ])
           ret = cluster.ReconfigureComputeResource_Task(spec: cluster_spec, modify: true).wait_for_completion
           rule = cluster.configurationEx.rule.find {|n| n[:name] == attributes[:name]}
@@ -32,7 +40,7 @@ module Fog
             raise Fog::Vsphere::Errors::ServiceError, "Unknown error creating rule #{attributes[:name]}"
           end
         end
-        
+
       end
       class Mock
         def create_rule(attributes={})
