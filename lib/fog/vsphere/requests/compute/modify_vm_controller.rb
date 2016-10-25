9710dd61cb30b88544263e4da1297878750bd205
c0puga <c0puga@walmart.com>
2016-10-25 08:38:33 -0500

    changing modify_vm_controller so that it follows the changes made to create_controller in 1.4.0

--- lib/fog/vsphere/requests/compute/modify_vm_controller.rb
+++ lib/fog/vsphere/requests/compute/modify_vm_controller.rb
@@ -3,25 +3,13 @@ module Fog
     class Vsphere
       class Real
         def add_vm_controller(controller)
-          options = {}
-          options[:operation] = 'add'
-          options[:type] = controller.type
-          options[:key] = controller.key
-          options[:shared] = controller.shared_bus
-
-          vm_reconfig_hardware('instance_uuid' => controller.server_id, 'hardware_spec' => {'deviceChange'=>[create_controller(options)]})
+          vm_reconfig_hardware('instance_uuid' => controller.server_id, 'hardware_spec' => {'deviceChange'=>[create_controller(controller)]})
         end
       end
 
       class Mock
         def add_vm_controller(controller)
-          options = {}
-          options[:operation] = 'add'
-          options[:type] = RbVmomi::VIM.VirtualLsiLogicController.class
-          options[:key] = 1000
-          options[:shared] = false
-
-          vm_reconfig_hardware('instance_uuid' => controller.server_id, 'hardware_spec' => {'deviceChange'=>[create_controller(options)]})
+          vm_reconfig_hardware('instance_uuid' => controller.server_id, 'hardware_spec' => {'deviceChange'=>[create_controller(controller)]})
         end
       end
     end
