f8b8759de44708a0e4e888da028b6cac6957fd7b
Derek Wright <derekmwright@users.noreply.github.com>
2017-08-31 12:33:28 -0400

    Updated list_clusters to have an optional param or allow filtering on datacenter (#102)
    Updated list_clusters to have an optional param or allow filtering on datacenter

--- tests/class_from_string_tests.rb
+++ tests/class_from_string_tests.rb
@@ -4,11 +4,11 @@ class A; class B; class C; end; end; end
 
 class TestClassFromString < Minitest::Test
   def test_empty_string
-    assert_equal(nil, Fog::Vsphere.class_from_string(''))
+    assert_nil(Fog::Vsphere.class_from_string(''))
   end
 
   def test_nil
-    assert_equal(nil, Fog::Vsphere.class_from_string(nil))
+    assert_nil(Fog::Vsphere.class_from_string(nil))
   end
 
   def test_name_as_class
@@ -20,7 +20,7 @@ class TestClassFromString < Minitest::Test
   end
 
   def test_unexpected_input
-    assert_equal(nil, Fog::Vsphere.class_from_string(8))
+    assert_nil(Fog::Vsphere.class_from_string(8))
   end
 
   def test_nested_class_without_default_path
