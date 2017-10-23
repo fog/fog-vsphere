59b1e5c7a57bf30b9078c2d5d876c905d24adbc3
Sergio Cambra <sergio@programatica.es>
2017-10-23 10:35:33 +0200

    remove empty lines

--- lib/fog/vsphere/requests/compute/host_finish_maintenance.rb
+++ lib/fog/vsphere/requests/compute/host_finish_maintenance.rb
@@ -2,14 +2,12 @@ module Fog
   module Compute
     class Vsphere
       class Real
-
         def host_finish_maintenance(name, cluster_name, datacenter_name, timeout = 0)
           host_ref = get_host(name, cluster_name, datacenter_name)
           task = host_ref.ExitMaintenanceMode_Task(:timeout => timeout)
           task.wait_for_completion
           { 'task_state' => task.info.state }
         end
-
       end
     end
   end
