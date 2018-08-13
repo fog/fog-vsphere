617f9b49059342f60c63d25a67db69c6b01314f6
daxen79 <yasensim@gmail.com>
2018-08-13 09:41:32 +0200

    Add NSX-T support in create_vm.rb

--- lib/fog/vsphere/requests/compute/create_vm.rb
+++ lib/fog/vsphere/requests/compute/create_vm.rb
@@ -171,6 +171,12 @@ module Fog
                 switchUuid: raw_network.config.distributedVirtualSwitch.uuid
               )
             )
+          elsif raw_network.is_a? RbVmomi::VIM::OpaqueNetwork
+            RbVmomi::VIM.VirtualEthernetCardOpaqueNetworkBackingInfo(
+              opaqueNetworkType: 'nsx.LogicalSwitch',
+              opaqueNetworkId: raw_network.summary.opaqueNetworkId
+            )
+
           else
             RbVmomi::VIM.VirtualEthernetCardNetworkBackingInfo(deviceName: nic.network)
           end
