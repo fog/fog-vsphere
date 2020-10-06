3a26960edabe1cbd469cbe031fea84169be8f01d
Sergio Cambra <sergio@enpijama.es>
2020-10-06 16:35:27 +0200

    verify template_datacenter exists

--- lib/fog/vsphere/requests/compute/vm_clone.rb
+++ lib/fog/vsphere/requests/compute/vm_clone.rb
@@ -21,6 +21,9 @@ module Fog
           end
           raise ArgumentError, 'cluster option is required' unless options['resource_pool'][0]
           raise Fog::Vsphere::Compute::NotFound, "Datacenter #{options['datacenter']} Doesn't Exist!" unless get_datacenter(options['datacenter'])
+          if options['template_datacenter'] && !get_datacenter(options['template_datacenter'])
+            raise Fog::Vsphere::Compute::NotFound, "Datacenter #{options['template_datacenter']} Doesn't Exist!"
+          end
           raise Fog::Vsphere::Compute::NotFound, "Template #{options['template_path']} Doesn't Exist!" unless get_virtual_machine(options['template_path'], options['template_datacenter'] || options['datacenter'])
           raise Fog::Vsphere::Compute::NotFound, "Cluster #{options['resource_pool'][0]} Doesn't Exist in the DC!" unless get_raw_cluster(options["resource_pool"][0], options['datacenter'])
           raise ArgumentError, 'path option is required' unless options.fetch('dest_folder', '/')
