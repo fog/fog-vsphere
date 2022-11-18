a83f25bbc93b41e23542dad97abe5880290be285
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-11-18 14:36:53 +0100

    Set server tests to pending
    It appears these tests never worked. I tried to rewrite them to correct
    shindo tests but then they failed. For now marked as pending.

--- tests/models/compute/servers_tests.rb
+++ tests/models/compute/servers_tests.rb
@@ -2,12 +2,13 @@ Shindo.tests('Fog::Compute[:vsphere] | servers collection', ['vsphere']) do
   servers = Fog::Compute[:vsphere].servers
 
   tests('The servers collection') do
+    pending # Not mocked
     test('should not be empty') { !servers.empty? }
-    test('should be a kind of Fog::Vsphere::Compute::Servers') { servers.is_a? Fog::Vsphere::Compute::Servers }
-    tests('should be able to reload itself').succeeds { servers.reload }
+    tests('should be a kind of Fog::Vsphere::Compute::Servers').returns(Fog::Vsphere::Compute::Servers) { servers }
+    test('should be able to reload itself') { servers.reload }
     tests('should be able to get a model') do
-      tests('by managed object reference').succeeds { servers.get 'jefftest' }
-      tests('by instance uuid').succeeds { servers.get '5032c8a5-9c5e-ba7a-3804-832a03e16381' }
+      tests('by managed object reference').returns(String) { servers.get('jefftest') }
+      tests('by instance uuid').returns(String) { servers.get('5032c8a5-9c5e-ba7a-3804-832a03e16381') }
     end
   end
 end
