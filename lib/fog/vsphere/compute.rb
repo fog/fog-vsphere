fog/fog-vsphere

commit d971ad5ea1ae8945acad353a57b75561993cab2e

Fix warning on nil comparison
This uses the more efficient /regex/.match?(value) syntax that also
doesn't raise any warning if id is nil.

lib/fog/vsphere/compute.rb
@@ -347,7 +347,7 @@ module Fog
         end
 
         def is_uuid?(id)
-          !(id =~ /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/).nil?
+          /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/.match?(id)
         end
       end
 

commit 514b76714c1751a2159dadaae043e67d74a869c8

Deal with non-strings in is_uuid?
In d971ad5ea1ae8945acad353a57b75561993cab2e the method was rewritten
with the intention of being compatible with Ruby 3.3 where =~ is only
defined on strings.

A problem with this is that /regex/.match?(nil) works, but
/regex/.match?(42) raises a TypeError. It would have been broken with
the old code on Ruby 3.3 as well, but only raised a deprecation warning
on prior versions.

Fixes: d971ad5ea1ae ("Fix warning on nil comparison")

lib/fog/vsphere/compute.rb
@@ -347,7 +347,7 @@ module Fog
         end
 
         def is_uuid?(id)
-          /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/.match?(id)
+          id.is_a?(String) && /[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}/.match?(id)
         end
       end
 